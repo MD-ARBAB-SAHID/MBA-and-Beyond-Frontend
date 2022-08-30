f065235e2ef4dcb44e383487a4379e32a560dd67
MD-ARBAB-SAHID <63171580+MD-ARBAB-SAHID@users.noreply.github.com>
2022-08-31 00:37:50 +0530

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container py-4">
         <div className="row gy-4 ">
           <div className={`col-lg-12 col-md-12 ${Styles["text-center"]}`}>
-            <h5 className="h1 text-white">MBA and Beyond Pvt. Ltd.</h5>
+            <h5 className="h1 text-white">Beyond Pvt. Ltd.</h5>
 
             <p className="large text-muted mb-0">
               &copy; Copyrights. All rights reserved.
